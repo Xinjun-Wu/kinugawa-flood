0c75db6f91968793ab21e4dc1a7d542697c6e4c4
dl-box <None>
2021-03-09 17:23:33 +0900

    train the model until BP032

--- runingfiles/Step1_PrepareData.bat
+++ runingfiles/Step1_PrepareData.bat
@@ -1,9 +1,80 @@
-python Csv2Npy.py BP021
-python generateData.py BP021
+@REM python Csv2Npy.py BP021
+@REM python generateData.py BP021
 
-python Csv2Npy.py BP020
-python generateData.py BP020
+@REM python Csv2Npy.py BP020
+@REM python generateData.py BP020
 
-python Csv2Npy.py BP022
-python generateData.py BP022
+@REM python Csv2Npy.py BP022
+@REM python generateData.py BP022
 
+@REM python Csv2Npy.py BP024
+@REM python generateData.py BP024
+
+@REM python Csv2Npy.py BP025
+@REM python generateData.py BP025
+
+@REM python Csv2Npy.py BP026
+@REM python generateData.py BP026
+
+@REM python Csv2Npy.py BP027
+@REM python generateData.py BP027
+
+@REM python Csv2Npy.py BP029
+@REM python generateData.py BP029
+
+@REM python Csv2Npy.py BP030
+@REM python generateData.py BP030
+
+@REM python Csv2Npy.py BP031
+@REM python generateData.py BP031
+
+@REM python Csv2Npy.py BP032
+@REM python generateData.py BP032
+
+@REM python Csv2Npy.py BP034
+@REM python generateData.py BP034
+
+@REM python Csv2Npy.py BP035
+@REM python generateData.py BP035
+
+@REM python Csv2Npy.py BP036
+@REM python generateData.py BP036
+
+@REM python Csv2Npy.py BP037
+@REM python generateData.py BP037
+
+@REM python Csv2Npy.py BP040
+@REM python generateData.py BP040
+
+@REM python Csv2Npy.py BP042
+@REM python generateData.py BP042
+
+@REM python Csv2Npy.py BP145
+@REM python generateData.py BP145
+
+@REM python Csv2Npy.py BP168
+@REM python generateData.py BP168
+
+@REM python Csv2Npy.py BP181
+@REM python generateData.py BP181
+
+python Csv2Npy.py BP006
+python generateData.py BP006
+
+python Csv2Npy.py BP010
+python generateData.py BP010
+
+python Csv2Npy.py BP012
+python generateData.py BP012
+
+python Csv2Npy.py BP015
+python generateData.py BP015
+
+python Csv2Npy.py BP016
+python generateData.py BP016
+
+python Csv2Npy.py BP017
+python generateData.py BP017
+
+python Csv2Npy.py BP019
+python generateData.py BP019
--- runingfiles/Step2_TrainModel.bat
+++ runingfiles/Step2_TrainModel.bat
@@ -1,3 +1,71 @@
-python TrainMethod_SingleBP.py BP021 128 0 6000 
-python TestMethod.py BP021 0 6000
-python TestVisualization.py BP021 5000 6000
+@REM python TrainMethod_SingleBP.py BP020 128 0 6000 
+@REM python TestMethod.py BP020 0 6000
+@REM python TestVisualization.py BP020 5000 6000
+
+@REM python TrainMethod_SingleBP.py BP021 128 6000 6000 
+@REM python TestMethod.py BP021 5000 6000
+@REM python TestVisualization.py BP021 5000 6000
+
+@REM python TrainMethod_SingleBP.py BP022 128 0 6000 
+@REM python TestMethod.py BP022 0 6000
+@REM python TestVisualization.py BP022 5000 6000
+
+@REM python TrainMethod_SingleBP.py BP024 128 5000 6000 
+@REM python TestMethod.py BP024 0 6000
+@REM python TestVisualization.py BP024 5000 6000
+
+@REM python TrainMethod_SingleBP.py BP025 128 0 6000 
+@REM python TestMethod.py BP025 0 6000
+@REM python TestVisualization.py BP025 5000 6000
+
+@REM python TrainMethod_SingleBP.py BP026 128 0 6000 
+@REM python TestMethod.py BP026 0 6000
+@REM python TestVisualization.py BP026 5000 6000
+
+@REM python TrainMethod_SingleBP.py BP027 128 0 6000 
+@REM python TestMethod.py BP027 0 6000
+@REM python TestVisualization.py BP027 5000 6000
+
+@REM python TrainMethod_SingleBP.py BP029 128 4300 6000 
+@REM python TestMethod.py BP029 0 6000
+@REM python TestVisualization.py BP029 5000 6000
+
+python TrainMethod_SingleBP.py BP006 128 1600 6000 
+python TestMethod.py BP006 0 6000
+python TestVisualization.py BP006 5000 6000
+
+python TrainMethod_SingleBP.py BP010 128 0 6000 
+python TestMethod.py BP010 0 6000
+python TestVisualization.py BP010 5000 6000
+
+python TrainMethod_SingleBP.py BP012 128 0 6000 
+python TestMethod.py BP012 0 6000
+python TestVisualization.py BP012 5000 6000
+
+python TrainMethod_SingleBP.py BP015 128 0 6000 
+python TestMethod.py BP015 0 6000
+python TestVisualization.py BP015 5000 6000
+
+python TrainMethod_SingleBP.py BP016 128 0 6000 
+python TestMethod.py BP016 0 6000
+python TestVisualization.py BP016 5000 6000
+
+python TrainMethod_SingleBP.py BP017 128 0 6000 
+python TestMethod.py BP017 0 6000
+python TestVisualization.py BP017 5000 6000
+
+python TrainMethod_SingleBP.py BP019 128 0 6000 
+python TestMethod.py BP019 0 6000
+python TestVisualization.py BP019 5000 6000
+
+python TrainMethod_SingleBP.py BP030 128 0 6000 
+python TestMethod.py BP030 0 6000
+python TestVisualization.py BP030 5000 6000
+
+python TrainMethod_SingleBP.py BP031 128 0 6000 
+python TestMethod.py BP031 0 6000
+python TestVisualization.py BP031 5000 6000
+
+python TrainMethod_SingleBP.py BP032 128 0 6000 
+python TestMethod.py BP032 0 6000
+python TestVisualization.py BP032 5000 6000
