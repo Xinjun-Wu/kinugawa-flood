29245c084f81a7cf44fe86db540a54c51640adf3
Xinjun-Wu <bent20140204@outlook.com>
2021-03-09 17:14:17 +0800

    init the parameters in academic branch

--- runingfiles/Step1_PrepareData.bat
+++ runingfiles/Step1_PrepareData.bat
@@ -1,9 +1,4 @@
-python Csv2Npy.py BP021
-python generateData.py BP021
+python Csv2Npy.py BP032
+python generateData.py BP032
 
-python Csv2Npy.py BP020
-python generateData.py BP020
-
-python Csv2Npy.py BP022
-python generateData.py BP022
 
--- runingfiles/Step2_TrainModel.bat
+++ runingfiles/Step2_TrainModel.bat
@@ -1,7 +1,4 @@
-python TrainMethod_SingleBP.py BP021 128 5000
-python TestMethod.py BP021
-python TestVisualization.py BP021
+python TrainMethod_SingleBP.py BP032 128 6000 12000
+python TestMethod.py BP032 0 12000
+python TestVisualization.py BP032 11000 12000
 
-python TrainMethod_SingleBP.py BP020 128 0
-python TestMethod.py BP020
-python TestVisualization.py BP020
